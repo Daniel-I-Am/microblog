7a251329f03e82176d5362b47668faacef6a879c
Daniel de Cloet <djdecloet@gmail.com>
2019-01-06 00:50:51 +0100

    Uploaded post form

---
+++ src/Form/PostType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author', TextType::class)
+            ->add('content', TextType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+}
